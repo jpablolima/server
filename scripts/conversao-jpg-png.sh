5dbaf825a33f90b3aaec6aff91df56d12f2719f1
João Pablo <jp.pablolima@gmail.com>
2021-07-31 19:45:55 -0300

    conversão de formato de imagem .jpg para png

---
+++ scripts/conversao-jpg-png.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+CAMINHO_IMAGENS=/home/pablo/Alura/server/scripts/imagens-livros
+convert $CAMINHO_IMAGENS/$1.png $CAMINHO_IMAGENS/$1.png
+convert $CAMINHO_IMAGENS/$2.png $CAMINHO_IMAGENS/$2.png
