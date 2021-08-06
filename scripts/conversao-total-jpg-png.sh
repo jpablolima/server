60b7c9c2b449eae422b6f3acd197a5f3b5ee3769
João Pablo <jp.pablolima@gmail.com>
2021-08-06 18:22:41 +0000

    saida da execução

--- scripts/conversao-total-jpg-png.sh
+++ scripts/conversao-total-jpg-png.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-cd /home/pablo/Alura/server/scripts/imagens-livros
+converte_imagem(){
+#cd /home/pablo/Alura/server/scripts/imagens-livros
+cd /opt/server/scripts/imagens-livros
 if [ ! -d png ]
 then 
 	mkdir png
@@ -8,7 +10,15 @@ fi
 
 for imagem in *.jpg
 do
-	imagem_sem_extensao=$(ls $imagem | awk -F. '{ print $1 }')
+	local imagem_sem_extensao=$(ls $imagem | awk -F. '{ print $1 }')
 	convert $imagem_sem_extensao.jpg png/$imagem_sem_extensao.png
 done
+}
+converte_imagem
+if [ $? -eq 0 ]
+then 
+	echo "Conversão realizada com sucesso"
+else
+	echo "Houve falha na conversão"
+fi
 
